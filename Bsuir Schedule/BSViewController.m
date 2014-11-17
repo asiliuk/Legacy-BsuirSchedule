65b86a7cc70764e53692afa4e0627ee3a660640e
Anton Siliuk <budgged@gmail.com>
2014-11-18 02:19:58 +0300

    staff

--- Bsuir Schedule/BSViewController.m
+++ Bsuir Schedule/BSViewController.m
@@ -8,42 +8,27 @@
 
 #import "BSViewController.h"
 #import "BSDataManager.h"
+#import "BSLecturer+Thumbnail.h"
+#import "NSString+Transiterate.h"
 
 
-@interface BSViewController ()
-
+@interface BSViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *days;
 @end
 
 @implementation BSViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     NSString *groupNumber = @"151004";
     [[BSDataManager sharedInstance] scheduleForGroupNumber:groupNumber withComplitionHandler:^{
-        NSArray *l = [[BSDataManager sharedInstance] lectures];
-        NSArray *s = [[BSDataManager sharedInstance] subjects];
-        NSArray *d = [[BSDataManager sharedInstance] days];
-        NSArray *aud = [[BSDataManager sharedInstance] auditories];
-        NSArray *pairs = [[BSDataManager sharedInstance] pairs];
-        NSArray *weeks = [[BSDataManager sharedInstance] weekNumbers];
-        BSDayOfWeek *day = d[0];
-        NSInteger weekNum = 4;
-        for (BSDayOfWeek *day in d) {
-            NSLog(@"---------------");
-            NSSortDescriptor *sortD = [NSSortDescriptor sortDescriptorWithKey:@"startTime" ascending:YES];
-            NSArray *pairs = [day.pairs sortedArrayUsingDescriptors:@[sortD]];
-            for (BSPair *p in pairs) {
-                NSString *weeks = @"";
-                for (BSWeekNumber *weekNumber in p.weeks) {
-                    weeks = [weeks stringByAppendingString:[NSString stringWithFormat:@" %ld",[weekNumber.weekNumber integerValue]]];
-                }
-                NSLog(@"%@ %@ %@ %@", p.subject.name, p.subjectType, p.lecturer.lastName, weeks);
-            }
-        }
-
+        self.days = [[BSDataManager sharedInstance] days];
+        [self.tableView reloadData];
     }];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"CellID"];
     
-    NSLog(@"%d",[[BSDataManager sharedInstance] scheduleNeedUpdate]);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -55,6 +40,42 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//===============================================TABLE VIEW===========================================
+#pragma mark - Table View
+
+- (NSArray*)pairsInDay:(BSDayOfWeek*)day forWeekNum:(NSInteger)weekNum {
+    BSWeekNumber *wn = [[BSDataManager sharedInstance] weekNumberWithNumber:weekNum createIfNotExists:YES];
+    NSSortDescriptor *sortD = [NSSortDescriptor sortDescriptorWithKey:@"startTime" ascending:YES];
+    NSArray *pairs = [day.pairs sortedArrayUsingDescriptors:@[sortD]];
+    NSMutableArray *weekPairs = [NSMutableArray array];
+    for (BSPair *pair in pairs) {
+        if ([pair.weeks containsObject:wn]) {
+            [weekPairs addObject:pair];
+        }
+    }
+    return weekPairs;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [self.days count];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[self pairsInDay:self.days[section] forWeekNum:4] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *c = [tableView dequeueReusableCellWithIdentifier:@"CellID" forIndexPath:indexPath];
+    BSPair *pair = [[self pairsInDay:self.days[indexPath.section] forWeekNum:4] objectAtIndex:indexPath.row];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"HH:mm"];
+    
+    NSString *dataString = [NSString stringWithFormat:@"%ld %@-%@ %@ (%@)", indexPath.section,[formatter stringFromDate:pair.startTime],[formatter stringFromDate:pair.endTime],pair.subject.name, pair.subjectType];
+    [c.textLabel setText:dataString];
+    c.imageView.image = [pair.lecturer thumbnail];
+    return c;
 }
 
 @end
