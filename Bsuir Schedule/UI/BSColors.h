e33bd0ca0e471c2eeaab831777f5537b8543782d
Anton Siliuk <budgged@gmail.com>
2014-11-20 04:34:40 +0300

    Fonts ad

---
+++ Bsuir Schedule/UI/BSColors.h
@@ -0,0 +1,20 @@
+//
+//  BSColors.h
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 20.11.14.
+//  Copyright (c) 2014 Saute. All rights reserved.
+//
+
+#ifndef Bsuir_Schedule_BSColors_h
+#define Bsuir_Schedule_BSColors_h
+
+#define UIColor(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#define BS_RED UIColor(241, 90, 90, 1.0)
+#define BS_GREEN UIColor(78, 186, 111, 1.0)
+#define BS_YELLOW UIColor(240, 197, 78, 1.0)
+#define BS_BLUE UIColor(50, 149, 191, 1.0)
+#define BS_PURPLE UIColor(149, 91, 165, 1.0)
+
+
+#endif
