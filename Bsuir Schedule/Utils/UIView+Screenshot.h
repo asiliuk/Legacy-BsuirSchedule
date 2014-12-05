bee2d3355dc6128fae015c8fcba411cfec86f8b7
Anton Siliuk <budgged@gmail.com>
2014-12-05 03:18:50 +0300

    Adding utils

---
+++ Bsuir Schedule/Utils/UIView+Screenshot.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Screenshot.h
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 05.12.14.
+//  Copyright (c) 2014 Saute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Screenshot)
+- (UIImage*)bluredScreenshot;
+@end
---
+++ Bsuir Schedule/Utils/UIView+Screenshot.m
@@ -0,0 +1,25 @@
+//
+//  UIView+Screenshot.m
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 05.12.14.
+//  Copyright (c) 2014 Saute. All rights reserved.
+//
+
+#import "UIView+Screenshot.h"
+#import "UIImage+ImageEffects.h"
+
+@implementation UIView (Screenshot)
+- (UIImage*)screenshot {
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, 0);
+    [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:YES];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (UIImage*)bluredScreenshot {
+    UIImage *screenshot = [self screenshot];
+    return [screenshot applyDarkEffect];
+}
+@end
