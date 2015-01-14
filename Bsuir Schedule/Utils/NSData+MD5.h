4282a31d2c41920c15ec276305a39d0f9ffe27f1
Anton Siliuk <budgged@gmail.com>
2015-01-15 00:58:58 +0300

    MD5 for data add

---
+++ Bsuir Schedule/Utils/NSData+MD5.h
@@ -0,0 +1,13 @@
+//
+//  NSData+MD5.h
+//  ContactSync
+//
+//  Created by Anton Siliuk on 12.09.14.
+//  Copyright (c) 2014 Saute. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+- (NSString*)MD5;
+@end
