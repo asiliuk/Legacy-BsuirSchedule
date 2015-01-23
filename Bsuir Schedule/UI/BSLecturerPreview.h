83766e32814faf093c384ad32326620de87955be
Anton Siliuk <budgged@gmail.com>
2015-01-23 20:25:49 +0300

    Lecture preview add

---
+++ Bsuir Schedule/UI/BSLecturerPreview.h
@@ -0,0 +1,19 @@
+//
+//  BMWLecturerPreview.h
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 23.01.15.
+//  Copyright (c) 2015 Saute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BSLecturer+Thumbnail.h"
+
+#define LECTURER_IMAGE_WIDTH 50.0f
+@interface BSLecturerPreview : UIView
+@property (strong, nonatomic) BSLecturer *lecturer;
+@property (strong, nonatomic) UIImageView *lecturerIV;
+@property (strong, nonatomic) UILabel *lecturerNameLabel;
+
+- (void)setupWithLecturer:(BSLecturer*)lecturer;
+@end
