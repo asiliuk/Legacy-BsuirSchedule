82623d1214dd482dd337c6a4bda50f42ea402ad0
Anton Siliuk <budgged@gmail.com>
2015-02-06 13:33:04 +0300

    code refactoring

---
+++ Bsuir Schedule/UI/Menu/BSMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  BSMenuVC.h
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 06.02.15.
+//  Copyright (c) 2015 Saute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSMenuVC : UIViewController
+
+@end
---
+++ Bsuir Schedule/UI/Menu/BSMenuVC.m
@@ -0,0 +1,27 @@
+//
+//  BSMenuVC.m
+//  Bsuir Schedule
+//
+//  Created by Anton Siliuk on 06.02.15.
+//  Copyright (c) 2015 Saute. All rights reserved.
+//
+
+#import "BSMenuVC.h"
+
+@interface BSMenuVC ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation BSMenuVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
