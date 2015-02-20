e03c326acc090f97af09b15cc95a534fab54614a
Anton Siliuk <budgged@gmail.com>
2015-02-20 18:09:07 +0300

    Button fix

--- Bsuir Schedule/AppDelegate.m
+++ Bsuir Schedule/AppDelegate.m
@@ -67,7 +67,7 @@
                                                                NSFontAttributeName: titleFont}];
     
     
-    UIButton *button  = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
+    UIButton *button  = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
     [button setImage:[UIImage imageNamed:@"menu_burger"] forState:UIControlStateNormal];
     [button addTarget:slideNavController action:@selector(toggleLeftMenu) forControlEvents:UIControlEventTouchUpInside];
     UIBarButtonItem *leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
