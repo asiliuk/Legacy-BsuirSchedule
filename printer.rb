4f9dccbc0ed623734c9693eef56f1b0e3ddd8eb2
Anton Siliuk <siliuk.anton@gmail.com>
2020-03-28 20:31:30 +0300

    Remove diplom prep staff

--- printer.rb
+++
@@ -1,8 +0,0 @@
-File.open('./res.txt', 'w') do |res|
-Dir[ File.join('./', '**', 'BS*.m') ].reject do |p| 
-  if !File.directory? p 
-    f = File.open(p, 'rb')
-    res.write(f.read)
-  end
-end
-end
